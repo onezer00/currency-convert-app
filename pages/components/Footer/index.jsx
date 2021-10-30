cb10a865a53023172d7885b1c31d3bf37eaa0ee3
onezer00 <caimbebr@gmail.com>
2021-10-30 19:51:00 -0300

    Remoção de link do footer.

--- pages/components/Footer/index.jsx
+++ pages/components/Footer/index.jsx
@@ -4,11 +4,11 @@ export default () => {
     return (
         <footer className="footer">
             <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="#"
             target="_blank"
             rel="noopener noreferrer"
             >
-            Powered by{' '} Oner
+            Powered by Oner
             </a>
         </footer>
     )
